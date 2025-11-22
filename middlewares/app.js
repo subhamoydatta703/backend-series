d4f8dc68e8802edb08ea1c9fd3965015f6b4cfaa
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-11-22 13:59:30 +0530

    Utility middlewares

--- middlewares/app.js
+++ middlewares/app.js
@@ -2,16 +2,29 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use((req, res, next) => {
-  console.log("middleware works");
+// app.use((req, res, next) => {
+//   console.log("middleware works");
 
-  next();
-});
+//   next();
+// });
 
-app.use((req, res, next) => {
-  console.log("middleware 2 works");
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("middleware 2 works");
+//   next();
+// });
+
+
+app.use((req, res, next)=>{
+    req.time= new Date(Date.now()).toString();
+    console.log(req.time);
+    console.log(req.method);
+    console.log(req.url);
+    console.log(req.hostname);
+    console.log(req.path);
+    next();
+    
+})
+
 app.get("/", (req, res) => {
   res.send("main rrot");
 });
