917823fffbd56672ae7d4d8b0bce79c4e842dd46
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-11-24 11:56:30 +0530

    Middlewares

--- middlewares/app.js
+++ middlewares/app.js
@@ -13,26 +13,37 @@ const path = require("path");
 //   next();
 // });
 
-
+// logger-> give us the information about every request
 app.use((req, res, next)=>{
     req.time= new Date(Date.now()).toString();
     console.log(req.time);
-    console.log(req.method);
-    console.log(req.url);
-    console.log(req.hostname);
+    // console.log(req.method);
+    // console.log(req.url);
+    // console.log(req.hostname);
     console.log(req.path);
     next();
     
 })
 
+
+const beforeRandom = (req, res, next)=>{
+console.log("This runs before random");
+next();
+
+}
+
 app.get("/", (req, res) => {
-  res.send("main rrot");
+  res.send("main route");
 });
 
-app.get("/random", (req, res) => {
+
+
+app.get("/random", beforeRandom, (req, res) => {
   res.send("Random route");
 });
 
 app.listen(8080, () => {
   console.log("listen at 8080");
 });
+
+
